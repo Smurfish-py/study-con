405a39924a3815d9088bf70c7f81e06ed7efd744
Smurfish-py <m.rifki0729@gmail.com>
2025-03-21 17:05:05 +0700

    Memperbaiki bug pada cara menyimpan nama gambar header kelas di database

--- logika/buat_kelas_logic.php
+++ logika/buat_kelas_logic.php
@@ -23,14 +23,24 @@ if (isset($_POST)) {
     $nama_header_baru = $id_kelas."_".$header_kelas_nama;
     $path = "../assets/images/kelas/";
 
-    $sql = "INSERT INTO kelas (id, id_guru, nama_kelas, password, deskripsi_kelas, gambar_header_kelas) VALUES (:id, :id_guru, :nama_kelas, :password, :deskripsi_kelas, :gambar_header_kelas)";
+    if (!empty($header_kelas_file)) {
+        $sql = "INSERT INTO kelas (id, id_guru, nama_kelas, password, deskripsi_kelas, gambar_header_kelas) VALUES (:id, :id_guru, :nama_kelas, :password, :deskripsi_kelas, :gambar_header_kelas)";
 
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(['id'=>$id_kelas,'id_guru'=>$id_guru, 'nama_kelas'=>$nama_kelas, 'password'=>$password_kelas, 'deskripsi_kelas'=>$desc_kelas, 'gambar_header_kelas'=>$nama_header_baru]);
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['id'=>$id_kelas,'id_guru'=>$id_guru, 'nama_kelas'=>$nama_kelas, 'password'=>$password_kelas, 'deskripsi_kelas'=>$desc_kelas, 'gambar_header_kelas'=>$nama_header_baru]);
 
-    move_uploaded_file($header_kelas_file, $path.$nama_header_baru);
+        move_uploaded_file($header_kelas_file, $path.$nama_header_baru);
 
-    header("location: multifunction_page/success.php?message=Berhasil%20membuat%20kelas%20dengan%20nama%20$nama_kelas%2E&link=user%2Fteacher%2F&type=Kembali%20ke%20dashboard");
+        header("location: multifunction_page/success.php?message=Berhasil%20membuat%20kelas%20dengan%20nama%20$nama_kelas%2E&link=user%2Fteacher%2F&type=Kembali%20ke%20dashboard");
+    } else {
+        $sql = "INSERT INTO kelas (id, id_guru, nama_kelas, password, deskripsi_kelas) VALUES (:id, :id_guru, :nama_kelas, :password, :deskripsi_kelas)";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['id'=>$id_kelas,'id_guru'=>$id_guru, 'nama_kelas'=>$nama_kelas, 'password'=>$password_kelas, 'deskripsi_kelas'=>$desc_kelas]);
+
+        header("location: multifunction_page/success.php?message=Berhasil%20membuat%20kelas%20dengan%20nama%20$nama_kelas%2E&link=user%2Fteacher%2F&type=Kembali%20ke%20dashboard");
+    }
+    
 } else {
     header("location: multifunction_page/error.php?message=Gagal%20membuat%20kelas%2E&link=user%2Fteacher%2F&type=Kembali");
 }
