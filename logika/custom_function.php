fe19096a7b1689ca6276da85c097b051226a2c6a
Smurfish-py <m.rifki0729@gmail.com>
2025-03-21 11:07:35 +0700

    Menambah fungsi generate id kelas.

--- logika/custom_function.php
+++ logika/custom_function.php
@@ -14,4 +14,16 @@ function ubahStatus_log($pelaku, $diubah, $status){
     $log_message = htmlspecialchars("$pelaku telah mengubah status $diubah menjadi $status", ENT_QUOTES, 'UTF-8');
     $stmt->execute([':log' => $log_message]);
 }
+
+function generateIdKelas($pdo) {
+    do {
+        $randomNumber = random_int(1000, 99999);
+
+        $stmt = $pdo->prepare("SELECT COUNT(*) FROM kelas WHERE id = ?");
+        $stmt->execute([$randomNumber]);
+        $exists = $stmt->fetchColumn();
+    } while ($exists > 0);
+
+    return $randomNumber;
+}
 ?>
