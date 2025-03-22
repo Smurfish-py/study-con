3cdfd7bbde54d9e79182ced702847609d8896f46
Smurfish-py <m.rifki0729@gmail.com>
2025-03-22 16:38:24 +0700

    Menambahkan header kelas

--- user/teacher/kelas.php
+++ user/teacher/kelas.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-include "../logika/koneksi.php";
+include "../../logika/koneksi.php";
 
 if (!isset($_SESSION['id'])) {
     session_destroy();
@@ -24,13 +24,13 @@ if (!isset($_SESSION['id'])) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/style.css">
 </head>
 <body>
     <header class="inter-400 header-dark" style="display: flex; justify-content: space-between; padding: 0 50px; align-items: center;">
         <div class="left-side" style="display: flex;">
             <?php
-            $path = "../assets/images/user/".$_SESSION['foto_profil'];
+            $path = "../../assets/images/user/".$_SESSION['foto_profil'];
             if(file_exists($path) != false && $_SESSION['foto_profil'] != ''){
                 echo "<div>";
                 echo "<img src='$path' style='width: 60px; height: 60px; object-fit: cover; border-radius: 100px; margin-right: 30px; margin-top: 12px;'>";
@@ -71,14 +71,39 @@ if (!isset($_SESSION['id'])) {
                 
             ?>
             <hr>
-            <a href="../logika/logout.php" class='font-size-md' style='text-decoration: none; color: #C00F0C;'>Logout from this device</a>
+            <a href="../../logika/logout.php" class='font-size-md' style='text-decoration: none; color: #C00F0C;'>Logout from this device</a>
         </div>
     </div>
-    <main>
+    <main style="display: flex; align-items: center; justify-content: center; padding-top: 105px; flex: 1;">
         <div class="class-container">
-            <div class="class-header">
+            <?php
+            if (isset($_GET['id'])) {
+                $idKelas = $_GET['id'];
+                $stmt = $pdo->prepare("SELECT * FROM kelas WHERE id=:id");
+                $stmt->execute(['id'=>$idKelas]);
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            </div>
+                $new_stmt = $pdo->prepare("SELECT * FROM user WHERE id=:id");
+                $new_stmt->execute(['id'=>$row['id_guru']]);
+                $new_row = $new_stmt->fetch(PDO::FETCH_ASSOC);
+
+                if ($row['gambar_header_kelas'] != '') {
+                    $gambarHeader = $row['gambar_header_kelas'];
+                } else {
+                    $gambarHeader = "default.jpg";
+                }
+                echo "<div class='class-header' style='display: flex; align-items: center; justify-content: space-between; background-image: url(\"../../assets/images/kelas/".$gambarHeader."\"); height: 160px; width: 100%; border-radius: 18px 18px 0 0; position: relative; background-repeat: no-repeat; background-size: cover; background-position: center;'>";
+                echo "<div style='display: flex;align-items: center; height: 100%; flex: 1;'>";
+                echo "<h1 class='inter-600' style='position: absolute; margin-left: 50px; color: white;'>".$row['nama_kelas']."<br><span class='inter-400' style='font-size: 20px; margin: 0;'>".$new_row['email']." (".$new_row['fullname'].")</span></h1>";
+                echo "</div>";
+                echo "<div style='height: 100%; flex: 1; display: flex; align-items: center; justify-content: right;'>";
+                echo "<a class='inter-400' href='index.php' style='color: white; position: absolute; text-decoration: none; margin-right: 50px;'>Kembali ke dashboard</a>";
+                echo "</div>";
+                echo "</div>";
+            }
+
+            
+            ?>
             <div class="class-body">
                 <div class="anggota-kelas">
                     
@@ -89,6 +114,6 @@ if (!isset($_SESSION['id'])) {
             </div>
         </div>
     </main>
-    <script src="../assets/javascript/scripts.js"></script>
+    <script src="../../assets/javascript/scripts.js"></script>
 </body>
 </html>
