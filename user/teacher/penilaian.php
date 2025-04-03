8cb86c8019e277ca31c16ff7d0bfe5fed8c3da1e
Smurfish-py <m.rifki0729@gmail.com>
2025-04-03 13:17:45 +0700

    Penambahan halaman penilaian beserta styling nya

--- user/teacher/penilaian.php
+++ user/teacher/penilaian.php
@@ -72,6 +72,122 @@ if (!isset($_SESSION['id'])) {
             <a href="../../logika/logout.php" class='font-size-md' style='text-decoration: none; color: #C00F0C;'>Logout from this device</a>
         </div>
     </div>
+    <main style="height: 99.5vh; display: flex; justify-content: center;">
+        <div class="tugas-nilai-container">
+            <?php
+            $id_tugas = $_GET['id_tugas'];
+            $id_kelas = $_GET['id_kelas'];
+            ?>
+            <div class="tugas-penilaian-section" id="tugas-penilaian-section">
+                <?php
+                $stmt = $pdo->prepare("SELECT * FROM guru_tugas WHERE id = :id AND id_kelas = :id_kelas");
+                $stmt->execute(['id'=>$id_tugas, ':id_kelas'=>$id_kelas]);
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                ?>
+                <h2 class="inter-600" style="margin: 0;"><?php echo $row['judul']?><br><span class="inter-400 font-size-l">Halaman penilaian dan komentar tugas</span></h2>
+                <hr style="margin: 0;">
+                <div class="tugas-penilaian-section-body">
+                    <div class="tugas-penilaian-section-list-pengirim-container">
+                        <div class="tugas-penilaian-list-section">
+                            <?php
+                            $list_pengirim = $pdo->prepare("SELECT * FROM user JOIN nilai_murid ON nilai_murid.id_murid = user.id WHERE nilai_murid.status = :status");
+                            $list_pengirim->execute([':status'=>'diserahkan']);
+                            echo "<hr style='margin-top: 0;'>";
+                            while ($pengirim_row = $list_pengirim->fetch(PDO::FETCH_ASSOC)) {
+                                ?>
+                                <a href="" class="tugas-penilaian-list-user">
+                                    <div class="tugas-penilaian-list-pengirim-inline">
+                                    <?php
+                                    if ($pengirim_row['foto_profil'] == '') {
+                                        echo "<i class='fa-solid fa-circle-user' style='font-size: 40px;'></i>";
+                                    } else {
+                                        echo "<img src='../../assets/images/user/".$pengirim_row['foto_profil']."' style='height: 40px; width: 40px; object-fit: cover; border-radius: 100px; border: 1px solid rgba(0, 0, 0, 0.3);'>";
+                                    }
+                                    ?>
+                                    <h3 class="inter-600" style="line-height: 1; margin-left: 10px;"><?php echo $pengirim_row['username']?><br><span class="inter-400 font-size-md">NIS: <?php echo $pengirim_row['NIS']?><br>Waktu pengumpulan: <?php echo $pengirim_row['waktu']?></span></h3>
+                                    </div>
+                                </a>
+                                <br>
+                                <hr>
+                                <?php
+                            }
+                            ?>
+                        </div>
+                        <div class="tugas-penilaian-komentar-button-section">
+                            <a href="" class="tugas-nilai-comment-button">
+                                <div class="tugas-nilai-comment-button-label">
+                                    <h3 class="inter-600">Buka komentar<span style="margin-left: 10px;"><i class="fa-solid fa-comments"></i></span></h3>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="tugas-penilaian-section-detail-tugas-container">
+                        <?php
+                        if (isset($_GET['id_user'])) {
+                            $id_user = $_GET['id_user'];
+                            ?>
+                            <div class="tugas-penilaian-detail-section">
+                                <?php
+                                $detail = $pdo->prepare("SELECT * FROM user JOIN nilai_murid ON nilai_murid.id_murid = user.id JOIN murid_tugas ON murid_tugas.id_file = nilai_murid.id_file WHERE user.id = :id");
+                                $detail->execute([':id'=>$id_user]);
+                                $detail_row = $detail->fetch(PDO::FETCH_ASSOC);
+                                ?>
+                                <div class="tugas-penilaian-user-profile">
+                                    <?php
+                                    if ($detail_row['foto_profil'] == '') {
+                                        echo "<i class='fa-solid fa-circle-user' style='font-size: 50px;'></i>";
+                                    } else {
+                                        ?>
+                                        <img src="../../assets/images/user/<?php echo $detail_row['foto_profil']?>" alt="user-profile" style="height: 50px; width: 50px; object-fit:cover; border-radius: 100px; border: 1px solid rgba(0, 0, 0, 0.3);">
+                                        <?php
+                                    }
+                                    ?>
+                                    <h3 class="inter-600" style='margin-left: 10px;'><?php echo $detail_row['username']?><br><span class="inter-400 font-size-l">NIS: <?php echo $detail_row['NIS']?>, Waktu pengiriman: <?php echo $detail_row['waktu']?></span></h3>
+                                </div>
+                                <div class="tugas-penilaian-lampiran">
+                                    <h3 class="inter-500">Lampiran tugas : </h3>
+                                    <?php
+                                    for ($i = 0; $i < $detail->rowCount(); $i++) {
+                                        $clean_name = explode("_", $detail_row['nama_file']);
+                                        ?>
+                                        <a class="inter-400" href="../../assets/documents/user/<?php echo $detail_row['nama_file']?>" target="_blank">-> <?php echo $clean_name[2]?></a><span class="inter-400"> (<a href="../../assets/documents/user/<?php echo $detail_row['nama_file']?>" download="<?php echo $clean_name[2]?>">Download</a>)</span>
+                                        <br>
+                                        <?php
+                                    }
+                                    ?>
+                                </div>
+                            </div>
+                            <div class="tugas-penilaian-nilai-section">
+                                <form action="" method="post">
+                                    <div class="tugas-penilaian-nilai-section-header">
+                                        <h3 class="inter-500" style="margin: 0;">Berikan komentar atau masukkan pribadi</h3>
+                                        <div class="nilai">
+                                            <label for="nilai-murid" class="inter-600">Beri nilai : </label>
+                                            <input class="inter-500" type="number" style="width: 50px; height: 20px;" name="nilai-murid" id="nilai-murid">
+                                        </div>
+                                    </div>
+                                    <div class="tugas-penilaian-nilai-section-body">
+                                        <textarea class="inter-400" name="masukan-pribadi" id="masukan-pribadi" style="min-height: 116px; max-height: 116px; min-width: 99.3%; max-width: 99.3%; margin-bottom: 7px;" placeholder="Masukkan komentar pribadi di sini..."></textarea>
+                                    </div>
+                                    <div class="tugas-penilaian-nilai-submit">
+                                        <button class="tugas-penilaian-nilai-submit-button" type="submit" style="height: 40px; width: 100%;">
+                                            <h2 class="inter-500" style="margin: 0;">Submit Penilaian <span style="margin-left: 10px;"><i class="fa-solid fa-check"></i></span></h2>
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                            <?php
+                        }
+                        ?>
+                    </div> 
+                </div>  
+            </div>
+            <div class="tugas-komentar-section" id="tugas-komentar-section" style="display: none;">
+
+            </div>
+        </div>
+    </main>
     <script src="../../assets/javascript/scripts.js"></script>
 </body>
 </html>
