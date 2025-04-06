03483a7bc40f1bb850b554a54d3bbb908f81e4ec
Smurfish-py <m.rifki0729@gmail.com>
2025-04-06 09:23:56 +0700

    Menambahkan halaman komentar untuk materi

---
+++ user/teacher/materi.php
@@ -0,0 +1,138 @@
+<?php
+session_start();
+
+include "../../logika/koneksi.php";
+
+if (!isset($_SESSION['id'])) {
+    header("Location: ../../login.php");
+    exit();
+} else if ($_SESSION['status'] != 'teacher' || $_SESSION['status-akun'] == 'banned') {
+    session_destroy();
+    header("Location: ../../");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StudyCon | Study and Connect With Everyone!</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+</head>
+<body>
+    <header class="inter-400 header-dark" style="display: flex; justify-content: space-between; padding: 0 50px; align-items: center;">
+        <div class="left-side" style="display: flex;">
+            <?php
+            $path = "../../assets/images/user/".$_SESSION['foto_profil'];
+            if(file_exists($path) != false && $_SESSION['foto_profil'] != ''){
+                echo "<div>";
+                echo "<img src='$path' style='width: 60px; height: 60px; object-fit: cover; border-radius: 100px; margin-right: 30px; margin-top: 12px;'>";
+                echo "</div>";
+                echo "<div>";
+                echo "<h3 class='inter-500'>".$_SESSION['username']."<br><span class='inter-300 font-size-l'>".$_SESSION['status']." account</span></h3>";
+                echo "</div>";
+            } else {
+                echo "<div style='display: flex; align-items: center; text-align: center; margin-right: 30px'><i class='fa-solid fa-circle-user fa-2xl' style='font-size: 60px' ;></i></div>";
+                echo "<div>";
+                echo "<h3 class='inter-500'>".$_SESSION['username']."<br><span class='inter-300 font-size-l'>".$_SESSION['status']." account</span></h3>";
+                echo "</div>";
+            }
+            ?>
+        </div>
+        <a id="user-menu">
+            <i class="fa-solid fa-address-card fa-2xl"></i>
+        </a>
+    </header>
+    <div class="inter-400 user-card" id="user-card" style="display: none; justify-content: center; ">
+        <p class="inter-500 font-size-l">Profile Card<br><span class="inter-200 font-size-md"><?php echo $_SESSION['username'];?></span></p>
+        <hr>
+        <br>
+        <div style="text-align: center;">
+            <?php
+            if(file_exists($path) != false && $_SESSION['foto_profil'] != ''){
+                echo "<img src='$path' style='width: 150px; height: 150px; object-fit: cover; border-radius: 100px; border: 1px solid rgba(0, 0, 0, 0.3);'>";
+                echo "<p class='font-size-l'>My Profile<br><span class='inter-300 font-size-md'><a href='../edit-profile.php' style='text-decoration: none; color: rgba(0, 0, 0, 0.8);'>Customize your profile</a></span></p>";
+            } else {
+                ?>
+                <div style="display: flex; align-items: center; justify-content: center; width: 220px; height: 150px; text-align: center; ">
+                    <i class='fa-solid fa-circle-user fa-2xl' style='font-size: 150px'></i>
+                </div>
+                    
+            <?php
+                echo "<p class='font-size-l'>My Profile<br><span class='inter-300 font-size-md'><a href='../edit-profile.php' style='text-decoration: none; color: rgba(0, 0, 0, 0.8);'>Customize your profile</a></span></p>";
+            }
+                
+            ?>
+            <hr>
+            <a href="../../logika/logout.php" class='font-size-md' style='text-decoration: none; color: #C00F0C;'>Logout from this device</a>
+        </div>
+    </div>
+    <main style="height: 99.5vh; display: flex; justify-content: center;">
+        <div class="tugas-nilai-container">
+            <?php
+            $id_tugas = $_GET['id_tugas'];
+            $id_kelas = $_GET['id_kelas'];
+            ?>
+            <div class="tugas-komentar-section" id="tugas-komentar-section">
+                <div class="tugas-komentar-section-body">
+                    <?php
+                    $id_kelas = $_GET['id_kelas'];
+                    $id_tugas = $_GET['id_tugas'];
+
+                    $stmt = $pdo->prepare("SELECT * FROM user JOIN komentar_tugas ON komentar_tugas.id_user = user.id WHERE komentar_tugas.id_tugas = :id_tugas AND komentar_tugas.id_kelas = :id_kelas");
+                    
+                    $stmt->execute([':id_tugas'=>$id_tugas, ':id_kelas'=>$id_kelas]);
+                    ?>
+                    <h1 class="inter-600 font-size-header" style="margin: 0;">Komentar Tugas <span class="inter-400 font-size-l">(<a href="kelas.php?id=<?php echo $_GET['id_kelas']?>" style="text-decoration: none;">Kembali ke halaman kelas</a>)</span><br><span class="inter-400 font-size-l"><?php ?></span></h1>
+                    <hr style="margin: 0 0 3px 0;">
+                    <div class="tugas-komentar-section-list-komentar" >
+                        <div class="tugas-komentar-section-list-komentar-section" id="scroll-area">
+                        <?php
+                        while ($komentar = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                            ?>
+                            <div class="tugas-komentar-section-komentar-user">
+                                <?php
+                                if ($komentar['foto_profil'] == '') {
+                                    echo "<i class='fa-solid fa-circle-user' style='font-size: 40px;'></i>";
+                                } else {
+                                    echo "<img src='../../assets/images/user/".$komentar['foto_profil']."' style='height: 40px; width: 40px; object-fit: cover; border-radius: 100px; border: 1px solid rgba(0, 0, 0, 0.3);'>";
+                                }
+
+                                ?>
+                                <h3 class="inter-600" style="margin-left: 15px;"><?php
+                                if ($komentar['status'] == 'teacher') {
+                                    echo $komentar['username']."<span class='inter-500 font-size-l' style='color: red;'> (Guru)</span>";
+                                } else {
+                                    echo $komentar['username'];
+                                }
+                                ?><br><span class="inter-400 font-size-l"><?php echo $komentar['isi']?></span></h3>
+                            </div>
+                            <hr style="margin: 0px;">
+                            <?php
+                        }
+                        ?>
+                        </div>
+                        <div class="tugas-komentar-section-kirim-komentar">
+                            <hr style="margin: 3px;">
+                            <form action="../../logika/penilaian_logic.php?id_tugas=<?php echo $_GET['id_tugas']?>&id_kelas=<?php echo $_GET['id_kelas']?>&action=kirim_komentar" method="post" class="tugas-komentar-section-kirim-komentar-input">
+                                <input type="text" class="inter-400 font-size-l tugas-komentar-section-kirim-komentar-input-text" placeholder="Masukkan komentar anda disini..." name="isi">
+                                <button type="submit" class="tugas-komentar-section-kirim-komentar-submit"><i class="fa-solid fa-paper-plane font-size-xl"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <script>
+        const scrollArea = document.getElementById('scroll-area');
+        scrollArea.scrollTop = scrollArea.scrollHeight;
+    </script>
+    <script src="../../assets/javascript/scripts.js"></script>
+</body>
+</html>
